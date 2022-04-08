09069f7d8abb1d0cefd891815364e10eaa9fcb7e
Harshal Jamdar <jamdarharshal@gmail.com>
2022-04-08 17:24:22 +0530

    8thApril

--- src/routes/route.js
+++ src/routes/route.js
@@ -13,7 +13,7 @@ router.get('/movies', function(req, res) {
 
 
 
-//problem 2:
+// //problem 2:
 // router.get( '/movies/:indexNumber', function (req, res) {
 
 //    let arr= ["PK","Taree Zamee Par","KKKG","DDLJ"]
@@ -36,7 +36,7 @@ res.send(getMovies())
  });
 
 
-//problem 4:
+// //problem 4:
 // router.get('/films',function(req,res){
 // let x=[ {
 //     'id': 1,
@@ -76,7 +76,7 @@ router.get('/films/:filmId', function(req, res) {
        if(req.params.filmId==x[i].id && x[i]!==undefined){
          var q=   x[i]
         } else if(req.params.filmId>x.length){
-           q= "No movie exists with this id"
+           q= "No movie exists with this given id"
         
         }
     
@@ -86,7 +86,61 @@ router.get('/films/:filmId', function(req, res) {
      res.send(getFilmId())  
 });
 
+//--------------------------------------------------------------------//
 
+
+// PRITESH SIR CLASS
+
+// router.get('/array', function(req, res) {
+//   let array1=[1,2,3,4,5,6,7,8,9]
+//   function getAddition(){
+//     let sum=0
+//   for (let i = 0; i < array1.length; i++) {
+//     sum += array1[i];
+//   }
+//   return sum
+// }
+// res.send(getAddition())
+// });
+
+
+
+
+// app.get("/sol1", function (req, res) {
+//   //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+//   let arr= [1,2,3,5,6,7]
+
+//   let total = 0;
+//   for (var i in arr) {
+//       total += arr[i];
+//   }
+
+//   let lastDigit= arr.pop()
+//   let consecutiveSum= lastDigit * (lastDigit+1) / 2
+//   let missingNumber= consecutiveSum - total
+
+//   res.send(  { data: missingNumber  }  );
+// });
+
+
+
+// app.get("/sol2", function (req, res) {
+//   //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+//   let arr= [33, 34, 35, 37, 38]
+//   let len= arr.length
+
+//   let total = 0;
+//   for (var i in arr) {
+//       total += arr[i];
+//   }
+
+//   let firstDigit= arr[0]
+//   let lastDigit= arr.pop()
+//   let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+//   let missingNumber= consecutiveSum - total
+ 
+//   res.send(  { data: missingNumber  }  );
+// });
 
 module.exports = router;
 // adding this comment for no reason
